f9de319034acb98fd0dd28d4541177b95eef4d2a
Blessing Kabungaidze <kblessingtatenda@gmail.com>
2024-07-27 01:38:12 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,20 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './routes'
 
+const sonar = await fetch(
+        "https://fixr-code.onrender.com/fix-code",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({
+            code: inputCode,
+            file_path: filePath,
+          }),
+        }
+      )
+
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.use(router)
