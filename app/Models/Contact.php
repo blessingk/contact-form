2955440629453ebe68b4934452ce0215873c2328
fixr <git@fixr.com>
2024-07-26 18:54:14 +0200

    Automated code changes

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -24,3 +24,31 @@ class Contact extends Model
         'content'
     ];
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact';
+
+    /**
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+
+    protected $fillable = [
+        'name',
+        'email',
+        'gender',
+        'content'
+    ];
+}
